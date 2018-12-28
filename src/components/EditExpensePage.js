46cd8e1d605e3590201bbbb72b8b557c76f17d8a
Chris Baucom <crbaucom@gmail.com>
2018-12-28 10:28:59 -0500

    fix edit route with url paramater

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
+const EditExpensePage = props => {
+  return (
+    <div>
+      This is from my edit expense component with id of {props.match.params.id}
+    </div>
+  );
+};
 
 export default EditExpensePage;
