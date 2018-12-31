dff0c2ebb43c2e6543e5438e5e6ce51877fe9747
Chris Baucom <crbaucom@gmail.com>
2018-12-30 19:37:12 -0500

    Setup test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyAeixRb-kTVgNmDmnG3sLSJzG6OqmLPm3c",
-  authDomain: "expensify-baucom.firebaseapp.com",
-  databaseURL: "https://expensify-baucom.firebaseio.com",
-  projectId: "expensify-baucom",
-  storageBucket: "expensify-baucom.appspot.com",
-  messagingSenderId: "309279973179"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
